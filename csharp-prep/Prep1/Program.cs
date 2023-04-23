d7d474dda7da69b27f7f4dfeff41a5ca48fad741
Matthew Huber <hub20012@byui.edu>
2023-04-22 18:48:43 -0600

    Completed Prep1 and Prep2 Assignments

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What's your first name? ");
+        string first_Name = Console.ReadLine();
+
+        Console.WriteLine("What's your last name?");
+        string last_Name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_Name}, {first_Name} {last_Name}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        
+
+        Console.WriteLine("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int percentage = int.Parse(grade);
+        
+        string letter = "";
+
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"This is your grade: {letter}");
+        
+        if (percentage < 70)
+        {
+            Console.WriteLine("You did not pass. Better luck next time!");
+
+        }
+
+        else
+        {
+            Console.WriteLine("Well done, you passed!");
+        }
     }
 }
