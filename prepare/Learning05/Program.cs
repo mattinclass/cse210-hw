b8b792436ee9d12d6e7196f6bc760a4d4a30dd0a
Matthew Huber <hub20012@byui.edu>
2023-06-21 18:26:10 -0600

    Completed this assignment.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+
+        Square mySquare = new Square("Blue", 5);
+        shapes.Add(mySquare);
+
+        Rectangle myRectangle = new Rectangle("Red", 5, 7);
+        shapes.Add(myRectangle);
+
+        Circle myCircle = new Circle("Yellow", 4);
+        shapes.Add(myCircle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The shape is {color} and it has an area of {area}.");
+        }
+        
     }
 }
